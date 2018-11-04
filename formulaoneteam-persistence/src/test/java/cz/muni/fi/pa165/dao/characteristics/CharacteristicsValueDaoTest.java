e7ba9147fb4118b064fcf41a0deb36cc3af4cb5f
Adel83 <adel.chakouri83@gmail.com>
2018-11-04 22:04:38 +0100

    TestDao

--- formulaoneteam-persistence/src/test/java/cz/muni/fi/pa165/dao/characteristics/CharacteristicsValueDaoTest.java
+++ formulaoneteam-persistence/src/test/java/cz/muni/fi/pa165/dao/characteristics/CharacteristicsValueDaoTest.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.dao.characteristics;
 import cz.muni.fi.pa165.AppContextConfig;
 import cz.muni.fi.pa165.entity.CharacteristicsType;
 import cz.muni.fi.pa165.entity.CharacteristicsValue;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,6 +16,15 @@ import org.testng.AssertJUnit;
 
 import javax.inject.Inject;
 
+import java.awt.*;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Adel Chakouri
+ */
+
 @ContextConfiguration(classes= AppContextConfig.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
 @Transactional
@@ -26,15 +36,58 @@ public class CharacteristicsValueDaoTest extends AbstractTestNGSpringContextTest
 
     @Test
     public void addCharacteristics_withValidData_isPersisted() {
-        //Given
-        CharacteristicsValue characteristicsValue = new CharacteristicsValue();
-        characteristicsValue.setValue(100.0);
-        characteristicsValue.setType(CharacteristicsType.AGGRESIVITY);
+        CharacteristicsType type = CharacteristicsType.AGGRESIVITY;
+        CharacteristicsValue characteristicsValue = createCharacteristics(type);
 
         //Then
         characteristicsValueDao.add(characteristicsValue);
 
         //That
         AssertJUnit.assertNotNull(characteristicsValueDao.findById(characteristicsValue.getId()));
+    }
+
+    @Test
+    public void deleteCharacteristics_withValidData_isPersisted() {
+
+        CharacteristicsType type = CharacteristicsType.AGGRESIVITY;
+        CharacteristicsValue characteristicsValue = createCharacteristics(type);
+
+        characteristicsValueDao.add(characteristicsValue);
+        characteristicsValueDao.delete(characteristicsValue);
+
+        List<CharacteristicsValue> result = (List<CharacteristicsValue>) characteristicsValueDao.findCharacteristicValuesByType(type);
+        assertEquals(0, result.size());
+
+    }
+
+    @Test
+    public void updateCharacteristics(){
+
+        CharacteristicsType oldType = CharacteristicsType.AGGRESIVITY;
+        CharacteristicsType newType = CharacteristicsType.PATIENCE;
+
+        CharacteristicsValue characteristicsValue = createCharacteristics(oldType);
+
+        characteristicsValueDao.add(characteristicsValue);
+
+        characteristicsValue.setType(newType);
+
+        List<CharacteristicsValue> oldCharacteristics =characteristicsValueDao.findCharacteristicValuesByType(oldType);
+        List<CharacteristicsValue> newCharacteristics =characteristicsValueDao.findCharacteristicValuesByType(newType);
+
+        assertEquals(0, oldCharacteristics.size());
+        assertEquals(1, newCharacteristics.size());
+
+    }
+
+
+
+    private CharacteristicsValue createCharacteristics(CharacteristicsType Type){
+        CharacteristicsValue characteristicsValue = new CharacteristicsValue();
+
+        characteristicsValue.setValue(100.0);
+        characteristicsValue.setType(Type);
+
+        return characteristicsValue;
     }
 }
--- formulaoneteam-persistence/src/test/java/cz/muni/fi/pa165/dao/driver/DriverDaoTest.java
+++ formulaoneteam-persistence/src/test/java/cz/muni/fi/pa165/dao/driver/DriverDaoTest.java
@@ -1,27 +1,99 @@
 package cz.muni.fi.pa165.dao.driver;
 
-import cz.muni.fi.pa165.dao.base.BaseTest;
+import cz.muni.fi.pa165.AppContextConfig;
 import cz.muni.fi.pa165.entity.Driver;
+import cz.muni.fi.pa165.entity.DriverStatus;
+import org.hibernate.annotations.Nationalized;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.AssertJUnit;
 
 import javax.inject.Inject;
+import java.util.Calendar;
+import java.util.Date;
 
-import static org.junit.Assert.assertNotNull;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
-public class DriverDaoTest extends BaseTest {
+/**
+ * @author Adel Chakouri
+ */
+
+@ContextConfiguration(classes= AppContextConfig.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DriverDaoTest extends AbstractTestNGSpringContextTests {
 
     @Inject
     private DriverDao driverDao;
 
     @Test
     public void addDriver_withValidData_isPersisted() {
-        //given
-        Driver driver = createDriver(getDefaultDriverEmail());
+        String mail ="Driver@drive.cz";
+        Driver driver = createDriver(mail);
 
         //when
         driverDao.add(driver);
 
         //then
-        assertNotNull(driverDao.findById(driver.getId()));
+        AssertJUnit.assertNotNull(driverDao.findById(driver.getId()));
+    }
+
+    @Test
+    public void deleteDriver_withValidData_isPersiste() {
+        String mail = "Driver@drive.cz";
+        Driver driver = createDriver(mail);
+
+        driverDao.add(driver);
+        driverDao.delete(driver);
+
+        Driver result = driverDao.findByEmail(mail);
+        assertNull(result);
+    }
+
+    @Test
+    public void updateDriver_withEmail() {
+
+       String oldMail="driver1@drive.cz";
+       String newMail="driver22@drive.Cz";
+
+       Driver driver = createDriver(oldMail);
+
+       driverDao.add(driver);
+
+       driver.setEmail(newMail);
+
+        Driver oldDriver = driverDao.findByEmail(oldMail);
+        Driver newDriver = driverDao.findByEmail(newMail);
+
+        assertNull(oldDriver);
+        assertNotNull(newDriver);
+    }
+
+
+
+    private Date createDate(int date, int month, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, date);
+        return calendar.getTime();
+    }
+    private Driver createDriver (String email){
+        Driver driver = new Driver();
+        driver.setBirthdate(createDate(2, 11, 1995));
+        driver.setName("name");
+        driver.setSurname("surname");
+        driver.setNationality("nationality");
+        driver.setDriverStatus(DriverStatus.MAIN);
+        driver.setPassword("password");
+        driver.setEmail(email);
+
+        return driver;
     }
 }
